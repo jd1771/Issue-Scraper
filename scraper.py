a42f0971ea54bcc9f9fd7ba6b19240e94769b4da
Jas Dhaul <jasdhaul0@gmail.com>
2023-06-23 20:51:08 -0700

    Now exporting to CSV and authenticating with GH

--- scraper.py
+++ scraper.py
@@ -1,14 +1,24 @@
 from github import Github
+from github import Auth
 from datetime import datetime
 import os
+import csv
+import time
+from dotenv import load_dotenv
 
-# Access the github token
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the GitHub token
 github_token = os.getenv("GITHUB_TOKEN")
 
-# Enter your GitHub personal access token here
-g = Github(github_token)
+# using an access token
+auth = Auth.Token(github_token)
 
-# List of the repo's of interest
+# Enter your GitHub personal access token here
+g = Github(auth=auth)
+
+# List of the repos of interest
 frameworks = [
     "facebook/react",
     "angular/angular",
@@ -17,23 +27,62 @@ frameworks = [
     "emberjs/ember.js",
     "preactjs/preact",
     "polymer/polymer",
-    "backbone/backbone",
+    "jashkenas/backbone",
     "knockout/knockout",
-    "mithriljs/mithril"
+    "MithrilJS/mithril.js"
 ]
 
-# Specify the repository
-repo = g.get_repo("facebook/react")
+## Subset of the repos of interest (for testing purposes)
+frameworks_subset = [
+    "facebook/react",
+    "angular/angular",
+    "vuejs/vue",
+]
 
-closed_issues = repo.get_issues(state='closed')[:50]
 
-count = 0
-avg_time_to_close = 0
-for issue in closed_issues:
-    if issue.closed_at is not None and issue.created_at is not None:
-        count += 1
-        time_to_close = issue.closed_at - issue.created_at
-        avg_time_to_close += time_to_close.total_seconds()
-        print(f"Issue {issue.number} was closed in {time_to_close.total_seconds()} seconds")
-print(f"Average time to close: {avg_time_to_close / count} seconds")
+# Prepare the CSV file
+csv_filename = "framework_stats.csv"
+csv_header = ["Framework", "Contributors", "Commits", "Issue Resolution Time"]
 
+with open(csv_filename, mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(csv_header)
+
+    # Loop through the frameworks list
+    for framework in frameworks_subset:
+        owner, repo = framework.split("/")
+        repo = g.get_repo(framework)
+
+        # Get the number of contributors
+        contributors = repo.get_contributors().totalCount
+
+        # Get the number of commits
+        commits = repo.get_commits().totalCount
+
+        # Get the average issue resolution time
+        issues = repo.get_issues(state="closed")[:50]
+        total_resolution_time = 0
+        total_resolved_issues = 0
+
+
+        count = 0
+        cumulative_time_to_close = 0
+        for issue in issues:
+            if issue.closed_at is not None and issue.created_at is not None:
+                count += 1
+                time_to_close = issue.closed_at - issue.created_at
+                cumulative_time_to_close += time_to_close.total_seconds()
+
+        avg_resolution_time = cumulative_time_to_close / count
+
+        # Write the data to the CSV file
+        data_row = [framework, contributors, commits, avg_resolution_time]
+        writer.writerow(data_row)
+
+        print(f"Data for {framework} written to CSV file.")
+
+        # Delay for a few seconds to avoid hitting the rate limits
+        time.sleep(2)
+
+
+print(f"Data exported to {csv_filename} successfully.")
